789a7274df69b2b4fb78c117781babf0caa79f2e
Lelouch0909 <lnhm87404@gmail.com>
2024-07-04 10:51:43 +0100

    Add new Spring project

---
+++ mindyback/src/main/java/com/enspd/mindyback/MindybackApplication.java
@@ -0,0 +1,13 @@
+package com.enspd.mindyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindybackApplication.class, args);
+	}
+
+}
---
+++ mindyback/src/test/java/com/enspd/mindyback/MindybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enspd.mindyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
